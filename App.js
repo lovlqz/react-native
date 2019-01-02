958703893ef8834181876d32a4e4df004a7537f1
liqizheng <qizheng.li@lantaiyuan.com>
2019-01-02 17:34:06 +0800

    底部bar

--- App.js
+++ App.js
@@ -20,10 +20,54 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style = {styles.container}>
+        <TabNavigator>
+          <TabNavigator.Item
+            selected = {this.state.selectedTab === 'tb_polular'}
+            title = '最热'
+            selectedTitleStyle={{color:'red'}}
+            renderIcon = {() => <Image style = {styles.botImage} source={require('./app/img/ic_polular.png')}/>}
+            renderSelectedIcon = {() => <Image style = {[styles.botImage, {tintColor: 'red'}]} source={require('./app/img/ic_polular.png')}/>}
+            badgeText='1'
+            onPress = {() => this.setState({selectedTab: 'tb_polular'})}>
+            <View style={styles.page1}>
+              
+            </View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected = {this.state.selectedTab === 'tb_trending'}
+            title = '趋势'
+            selectedTitleStyle={{color:'blue'}}
+            renderIcon = {() => <Image style = {styles.botImage} source={require('./app/img/ic_trending.png')}/>}
+            renderSelectedIcon = {() => <Image style = {[styles.botImage, {tintColor: 'blue'}]} source={require('./app/img/ic_trending.png')}/>}
+            onPress = {() => this.setState({selectedTab: 'tb_trending'})}>
+            <View style={styles.page2}>
+
+            </View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected = {this.state.selectedTab === 'tb_favorite'}
+            title = '收藏'
+            selectedTitleStyle={{color:'green'}}
+            renderIcon = {() => <Image style = {styles.botImage} source={require('./app/img/ic_favorite.png')}/>}
+            renderSelectedIcon = {() => <Image style = {[styles.botImage, {tintColor: 'green'}]} source={require('./app/img/ic_favorite.png')}/>}
+            onPress = {() => this.setState({selectedTab: 'tb_favorite'})}>
+            <View style={styles.page3}>
+
+            </View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected = {this.state.selectedTab === 'tb_my'}
+            title = '我的'
+            selectedTitleStyle={{color:'yellow'}}
+            renderIcon = {() => <Image style = {styles.botImage} source={require('./app/img/ic_my.png')}/>}
+            renderSelectedIcon = {() => <Image style = {[styles.botImage, {tintColor: 'yellow'}]} source={require('./app/img/ic_my.png')}/>}
+            onPress = {() => this.setState({selectedTab: 'tb_my'})}>
+            <View style={styles.page4}>
+
+            </View>
+          </TabNavigator.Item>
+        </TabNavigator>
       </View>
     );
   }
